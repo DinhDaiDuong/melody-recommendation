90bb49d46c45d986c7efa8987e02ebc19eac7d98
Daiduong1593572468 <daiduong35578@gmail.com>
2025-06-09 12:31:38 +0700

    update

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install system dependencies
+apt-get update
+apt-get install -y build-essential python3-dev
+
+# Install Python packages
 pip install --upgrade pip
+pip install wheel
 pip install -r requirements.txt 
