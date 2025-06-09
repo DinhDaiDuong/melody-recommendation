e1c8aee092fbaf08a5621beb25b929f5b4d9e663
Daiduong1593572468 <daiduong35578@gmail.com>
2025-06-09 12:48:02 +0700

    update2

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -o errexit
 
 # Install system dependencies
 apt-get update
-apt-get install -y build-essential python3-dev
+apt-get install -y build-essential python3-dev gcc
 
 # Install Python packages
 pip install --upgrade pip
 pip install wheel
+pip install cython==0.29.24
 pip install -r requirements.txt 
